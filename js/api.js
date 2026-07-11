b517253e56ab4a8be26436647eee0307e07e79bf
viplaminh93-cloud <viplaminh93@gmail.com>
2026-07-11 22:32:07 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -31,11 +31,16 @@ function taoRequest(maso){
 
 
 
+
+
+
+
+
 //======================================
 // NHẬN QR TỪ CAMERA
 //======================================
 
-window.onQRCode = function(maso){
+window.onQRCode = async function(maso){
 
     const request = taoRequest(maso);
 
@@ -43,9 +48,67 @@ window.onQRCode = function(maso){
 
 };
 
+
+
+
+
+
+
+
 //======================================
 // GỬI REQUEST
 //======================================
+
+async function guiRequest(request){
+
+    try{
+
+        const res = await fetch(
+
+            API_URL,
+
+            {
+
+                method:"POST",
+
+                redirect:"follow",
+
+                headers:{
+
+                    "Content-Type":"text/plain;charset=utf-8"
+
+                },
+
+                body: JSON.stringify(request)
+
+            }
+
+        );
+
+        return await res.json();
+
+    }
+
+    catch(err){
+
+        console.error(err);
+
+        return{
+
+            success:false,
+
+            message:"Không gửi được dữ liệu."
+
+        };
+
+    }
+
+}
+
+
+
+
+
 
 
 
