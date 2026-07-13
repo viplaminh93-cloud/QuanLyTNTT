0e3fe4adc13f05a95bfd3d352506b52ed52da5fc
viplaminh93-cloud <viplaminh93@gmail.com>
2026-07-13 21:55:04 +0700

    Update debug.js

--- js/debug.js
+++ js/debug.js
@@ -1,1 +1,46 @@
-a
+//======================================
+// DEBUG
+//======================================
+
+"use strict";
+
+const Debug = {
+
+    logs:[]
+
+};
+
+
+//======================================
+// ADD LOG
+//======================================
+
+function debug(message){
+
+    if(!CONFIG.APP.DEBUG){
+
+        return;
+
+    }
+
+    const now = new Date();
+
+    const time =
+
+        now.toLocaleTimeString();
+
+    const text =
+
+        "["
+
+        + time
+
+        + "] "
+
+        + message;
+
+    Debug.logs.push(text);
+
+    console.log(text);
+
+}
