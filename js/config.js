0b6baf5918dff976f32438673f4befca9b3b23fe
viplaminh93-cloud <viplaminh93@gmail.com>
2026-07-14 20:11:14 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -28,7 +28,7 @@ const CONFIG = Object.freeze({
     API: Object.freeze({
 
         URL:
-        "https://script.google.com/macros/s/AKfycbynm3_GmY5Tu9JrgTAIRk20cUTfMVpHKBQ5EYc4ZkIoZSkSX90Ov_T0Emmpr8CANaRGZw/exec"
+        "https://script.google.com/macros/s/AKfycbxpaFiH3GAd2TwAY2Q7R5d_fzd47rn5Ki9W60LqDJPBA6ROYMlcsYWCUtnDUFNwagz9pQ/exec"
 
     }),
 
